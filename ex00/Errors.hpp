9760f50083432c33be4dc860d5b4f02dd7937e37
17150012 <17150012@bjtu.edu.cn>
2018-07-12 23:58:00 +0800

    23:57

--- ex00/Errors.hpp
+++ ex00/Errors.hpp
@@ -2,62 +2,44 @@
 #ifndef ERRORS_HPP_/* dream0630 */
 #define ERRORS_HPP_/* dream0630 */
 /* dream0630 */
+#include <stdexcept>/* dream0630 */
 #include <string>/* dream0630 */
 /* dream0630 */
-class NasaError : public std::exception/* dream0630 */
-{/* dream0630 */
+class NasaError : public std::exception {/* dream0630 */
 public:/* dream0630 */
-  NasaError(std::string const &message,/* dream0630 */
-	    std::string const &component = "Unknown");/* dream0630 */
-  /* dream0630 */
-  std::string const &getComponent() const;/* dream0630 */
+	NasaError(std::string const& message,/* dream0630 */
+		std::string const& component = "Unknown");/* dream0630 */
 /* dream0630 */
-  virtual ~NasaError() throw();/* dream0630 */
-  const char*	what() const noexcept;/* dream0630 */
-  /* dream0630 */
+	std::string const& getComponent() const;/* dream0630 */
+	const char *what() const noexcept;/* dream0630 */
+/* dream0630 */
 private:/* dream0630 */
-  /* dream0630 */
-  std::string _message;/* dream0630 */
-  std::string _component;/* dream0630 */
-  /* dream0630 */
-  /* dream0630 */
+	std::string _message;/* dream0630 */
+	std::string _component;/* dream0630 */
 };/* dream0630 */
 /* dream0630 */
-class MissionCriticalError : public NasaError/* dream0630 */
-{/* dream0630 */
-    public:/* dream0630 */
-        MissionCriticalError(std::string const &message,/* dream0630 */
-                std::string const &component = "Unknown");/* dream0630 */
-/* dream0630 */
-  virtual ~MissionCriticalError() throw();/* dream0630 */
-  /* dream0630 */
-};/* dream0630 */
-/* dream0630 */
-class LifeCriticalError : public NasaError/* dream0630 */
-{/* dream0630 */
-    public:/* dream0630 */
-        LifeCriticalError(std::string const &message,/* dream0630 */
-                std::string const &component = "Unknown");/* dream0630 */
-  virtual ~LifeCriticalError() throw();/* dream0630 */
-  /* dream0630 */
-};/* dream0630 */
-/* dream0630 */
-class UserError : public NasaError/* dream0630 */
-{/* dream0630 */
+class LifeCriticalError : public NasaError {/* dream0630 */
 public:/* dream0630 */
-  UserError(std::string const &message,/* dream0630 */
-	    std::string const &component = "Unknown");/* dream0630 */
-/* dream0630 */
-  virtual ~UserError() throw();/* dream0630 */
+	LifeCriticalError(std::string const& message,/* dream0630 */
+		std::string const& component = "Unknown");/* dream0630 */
 };/* dream0630 */
 /* dream0630 */
-class CommunicationError : public NasaError/* dream0630 */
-{/* dream0630 */
+class MissionCriticalError : public NasaError {/* dream0630 */
 public:/* dream0630 */
-  CommunicationError(std::string const &message);/* dream0630 */
-  /* dream0630 */
-  virtual ~CommunicationError() throw();/* dream0630 */
-  /* dream0630 */
+	MissionCriticalError(std::string const& message,/* dream0630 */
+		std::string const& component = "Unknown");/* dream0630 */
 };/* dream0630 */
 /* dream0630 */
-#endif/* dream0630 */
+class CommunicationError : public NasaError {/* dream0630 */
+public:/* dream0630 */
+	CommunicationError(std::string const& message);/* dream0630 */
+};/* dream0630 */
+/* dream0630 */
+class UserError : public NasaError {/* dream0630 */
+public:/* dream0630 */
+	UserError(std::string const& message,/* dream0630 */
+		std::string const& component = "Unknown");/* dream0630 */
+};/* dream0630 */
+/* dream0630 */
+#endif /* dream0630 */
+/* dream0630 */
