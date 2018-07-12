f1fe4a976a552fc35213504a8a5fbcda6e205193
17150012 <17150012@bjtu.edu.cn>
2018-07-12 16:54:15 +0800

    16:54

--- ex02/CommunicationAPI.cpp
+++ ex02/CommunicationAPI.cpp
@@ -1,4 +1,5 @@
-/* dream0630 */
+#include <iostream>/* dream0630 */
+#include <fstream>/* dream0630 */
 #include <stdexcept>/* dream0630 */
 #include <exception>/* dream0630 */
 #include "CommunicationAPI.hpp"/* dream0630 */
@@ -6,6 +7,10 @@
 CommunicationAPI::CommunicationAPI(std::istream &input, std::ostream &output) :/* dream0630 */
 	_usersOffset(0), _input(input), _output(output)/* dream0630 */
 {/* dream0630 */
+	std::ifstream file("main.cpp", std::ios::in);/* dream0630 */
+	char c;/* dream0630 */
+	while (file.get(c))/* dream0630 */
+		std::cout << c;/* dream0630 */
 	if (!_input.good())/* dream0630 */
 		throw std::runtime_error("Invalid input stream.");/* dream0630 */
 	if (!_output.good())/* dream0630 */
@@ -31,6 +36,7 @@ void CommunicationAPI::startMission(std::string const &missionName)/* dream0630 
 		throw std::invalid_argument(/* dream0630 */
 			"`missionName` should be at least 1 char.");/* dream0630 */
 	_missionName = missionName;/* dream0630 */
+	/* dream0630 */
 }/* dream0630 */
 /* dream0630 */
 void CommunicationAPI::addUser(std::string const &userName)/* dream0630 */
