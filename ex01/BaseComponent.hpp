8e1ea1bfb12b7cc93a51ee9ffe88a32fb1f95506
17150012 <17150012@bjtu.edu.cn>
2018-07-12 23:59:04 +0800

    23:59

--- ex01/BaseComponent.hpp
+++ ex01/BaseComponent.hpp
@@ -2,10 +2,10 @@
 #ifndef BASE_COMPONENT_HPP_/* dream0630 */
 #define BASE_COMPONENT_HPP_/* dream0630 */
 /* dream0630 */
-class BaseComponent/* dream0630 */
-{/* dream0630 */
-    public:/* dream0630 */
-        virtual ~BaseComponent();/* dream0630 */
+class BaseComponent {/* dream0630 */
+public:/* dream0630 */
+	virtual ~BaseComponent() {}/* dream0630 */
 };/* dream0630 */
 /* dream0630 */
 #endif /* dream0630 */
+/* dream0630 */
