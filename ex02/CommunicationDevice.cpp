df01f3f6f39b0cb586faca318c42412082db4204
17150012 <17150012@bjtu.edu.cn>
2018-07-13 08:59:45 +0800

    8:59

--- ex02/CommunicationDevice.cpp
+++ ex02/CommunicationDevice.cpp
@@ -1,56 +1,69 @@
-/* dream0630 */
-#include <cstring>/* dream0630 */
 #include <iostream>/* dream0630 */
 #include <stdexcept>/* dream0630 */
+#include <sstream>/* dream0630 */
 #include "Errors.hpp"/* dream0630 */
 #include "CommunicationDevice.hpp"/* dream0630 */
 /* dream0630 */
-CommunicationDevice::CommunicationDevice(/* dream0630 */
-	std::istream &input, std::ostream &output)/* dream0630 */
-try : _api(input, output)/* dream0630 */
+CommunicationDevice::CommunicationDevice(std::istream &input,/* dream0630 */
+                                         std::ostream &output)/* dream0630 */
+   try : _api(input, output)/* dream0630 */
 {/* dream0630 */
-} catch (std::exception const &ex) {/* dream0630 */
-	throw CommunicationError(std::string("Error: ") + ex.what());/* dream0630 */
+}/* dream0630 */
+catch(std::exception &e) {/* dream0630 */
+std::stringstream out;/* dream0630 */
+/* dream0630 */
+out << "Error: " << e.what();/* dream0630 */
+throw CommunicationError(out.str());/* dream0630 */
 }/* dream0630 */
 /* dream0630 */
 CommunicationDevice::~CommunicationDevice()/* dream0630 */
 {/* dream0630 */
 }/* dream0630 */
 /* dream0630 */
-void CommunicationDevice::startMission(/* dream0630 */
-	std::string const &missionName, std::string *users, size_t nbUsers)/* dream0630 */
+void CommunicationDevice::startMission(std::string const &missionName,/* dream0630 */
+                                  std::string *users,/* dream0630 */
+                                  size_t nbUsers)/* dream0630 */
 {/* dream0630 */
-	try {/* dream0630 */
-		for (size_t i = 0; i < nbUsers; ++i)/* dream0630 */
-			_api.addUser(users[i]);/* dream0630 */
-		_api.startMission(missionName);/* dream0630 */
-	} catch (std::logic_error const &ex) {/* dream0630 */
-		throw (std::string("LogicError: ") + ex.what());/* dream0630 */
-	} catch (std::runtime_error const &ex) {/* dream0630 */
-		throw (std::string("RuntimeError: ") + ex.what());/* dream0630 */
-	} catch (std::exception const &ex) {/* dream0630 */
-		throw (std::string("Error: ") + ex.what());/* dream0630 */
-	}/* dream0630 */
+  std::stringstream out;/* dream0630 */
+/* dream0630 */
+  try {/* dream0630 */
+    for (size_t i = 0; i < nbUsers; ++i)/* dream0630 */
+      _api.addUser(users[i]);/* dream0630 */
+    _api.startMission(missionName);/* dream0630 */
+  }/* dream0630 */
+  catch(std::logic_error &e) {/* dream0630 */
+    out << "LogicError: " << e.what();/* dream0630 */
+    throw CommunicationError(out.str());/* dream0630 */
+  }/* dream0630 */
+  catch(std::runtime_error &e) {/* dream0630 */
+    out << "RuntimeError: " << e.what();/* dream0630 */
+    throw CommunicationError(out.str());/* dream0630 */
+  }/* dream0630 */
+  catch(std::exception &e) {/* dream0630 */
+    out << "Error: " << e.what();/* dream0630 */
+    throw CommunicationError(out.str());/* dream0630 */
+  }/* dream0630 */
 }/* dream0630 */
 /* dream0630 */
-void CommunicationDevice::send(/* dream0630 */
-	std::string const &user, std::string const &message) const/* dream0630 */
+void CommunicationDevice::send(std::string const &user,/* dream0630 */
+                          std::string const &message) const/* dream0630 */
 {/* dream0630 */
-	try {/* dream0630 */
-		_api.sendMessage(user, message);/* dream0630 */
-	} catch (std::exception const &ex) {/* dream0630 */
-		std::cerr << ex.what() << std::endl;/* dream0630 */
-	}/* dream0630 */
+  try {/* dream0630 */
+    _api.sendMessage(user, message);/* dream0630 */
+  }/* dream0630 */
+  catch(std::exception &e) {/* dream0630 */
+    std::cerr << e.what() << std::endl;/* dream0630 */
+  }/* dream0630 */
 }/* dream0630 */
 /* dream0630 */
-void CommunicationDevice::receive(/* dream0630 */
-	std::string const &user, std::string &message) const/* dream0630 */
+void CommunicationDevice::receive(std::string const &user,/* dream0630 */
+                             std::string &message) const/* dream0630 */
 {/* dream0630 */
-	try {/* dream0630 */
-		_api.receiveMessage(user, message);/* dream0630 */
-	} catch (std::exception const &ex) {/* dream0630 */
-		if (std::strcmp(ex.what(), "INVALID MESSAGE") == 0)/* dream0630 */
-			std::cerr << "INVALID MESSAGE" << std::endl;/* dream0630 */
-	}/* dream0630 */
+  try {/* dream0630 */
+    _api.receiveMessage(user, message);/* dream0630 */
+  }/* dream0630 */
+  catch (std::exception &e) {/* dream0630 */
+    std::cerr << e.what() << std::endl;/* dream0630 */
+    std::cerr << "INVALID MESSAGE" << std::endl;/* dream0630 */
+  }/* dream0630 */
 }/* dream0630 */
-/* dream0630 */
